80f8a33d92ae7777b8e2790be2b8e43e21538fa2
IFTI_AHMED <iftiahmed1997@gmail.com>
2023-03-18 10:22:27 +0600

    first commit

---
+++ src/main/java/com/example/ConsultingApp/ConsultingAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.ConsultingApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConsultingAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConsultingAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/ConsultingApp/ConsultingAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ConsultingApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConsultingAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
