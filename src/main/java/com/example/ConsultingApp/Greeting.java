11d00bf07f03722c47ef6212b6e2e5efbd354d36
IFTI_AHMED <iftiahmed1997@gmail.com>
2023-03-18 13:25:25 +0600

    3rd Commit

--- src/main/java/com/example/ConsultingApp/Greeting.java
+++ src/main/java/com/example/ConsultingApp/Greeting.java
@@ -2,6 +2,8 @@ package com.example.ConsultingApp;
 
 public class Greeting {
     private long id;
+    private String name;
+    private String email;
     private String content;
 
     public long getId() {
@@ -10,6 +12,22 @@ public class Greeting {
 
     public void setId(long id) {
         this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
     }
 
     public String getContent() {
